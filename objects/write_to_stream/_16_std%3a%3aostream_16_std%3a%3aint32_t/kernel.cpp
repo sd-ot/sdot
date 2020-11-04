3fdb43a46e2b6552dd4b91313afa2124fe047bdc
Hugo Leclerc <hugal.leclerc@gmail.com>
2020-11-04 18:26:27 +0100

    .

--- objects/write_to_stream/_16_std%3a%3aostream_16_std%3a%3aint32_t/kernel.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../../kernels/write_to_stream.h"
-
-extern "C" void kernel_wrapper( void **data ) {
-    write_to_stream(
-        *reinterpret_cast<std::ostream*>( data[ 0 ] ),
-        *reinterpret_cast<std::int32_t*>( data[ 1 ] )
-    );
-}
