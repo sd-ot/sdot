7bb9c2e1a99e682d4bc0f2d396933c6907b9953f
Hugo Leclerc <hugal.leclerc@gmail.com>
2020-11-07 08:45:36 +0100

    .

--- src/parex/Task.h
+++ src/parex/Task.h
@@ -14,27 +14,32 @@ class Value;
 */
 class Task {
 public:
-    /**/                           Task                 () { computed = false; in_front = false; kernel = nullptr; cpt_use = 0; op_id = 0; }
-    /**/                          ~Task                 ();
+    /**/                             Task                 () { computed = false; in_front = false; kernel = nullptr; cpt_use = 0; op_id = 0; }
+    /**/                            ~Task                 ();
 
 
-    template<class T> static Task* owning               ( T *ptr ); ///< Wrap a known source value. Takes ownership of ptr
-    static Task*                   call                 ( Kernel *kernel, std::vector<TaskRef> &&children = {} );
+    template<class T> static Task*   owning               ( T *ptr ); ///< Wrap a known source value. Takes ownership of ptr
+    static Task*                     call                 ( Kernel *kernel, std::vector<TaskRef> &&children = {} );
 
-    bool                           children_are_computed() const;
-    void                           get_front_rec        ( std::vector<Task *> &front );
+    bool                             children_are_computed() const;
+    void                             get_front_rec        ( std::vector<Task *> &front );
 
-    bool                           in_front;
-    bool                           computed;
-    std::vector<Output>            outputs;
+    template<class F,class...A> void run                  ( const F &func, A& ...args );
 
-    std::vector<TaskRef>           children;
-    std::vector<Task *>            parents;
-    Kernel*                        kernel;
+    template<class F,class...A> void run_void_or_not      ( std::integral_constant<bool,0>, const F &func, A& ...args );
+    template<class F,class...A> void run_void_or_not      ( std::integral_constant<bool,1>, const F &func, A& ...args );
 
-    static  std::size_t            curr_op_id;
-    mutable std::size_t            cpt_use;
-    mutable std::size_t            op_id;
+    bool                             in_front;
+    bool                             computed;
+    std::vector<Output>              outputs;
+
+    std::vector<TaskRef>             children;
+    std::vector<Task *>              parents;
+    Kernel*                          kernel;
+
+    static  std::size_t              curr_op_id;
+    mutable std::size_t              cpt_use;
+    mutable std::size_t              op_id;
 };
 
 template<class T>
@@ -43,6 +48,23 @@ Task *Task::owning( T *ptr ) {
     res->outputs.push_back( Output{ type_name( ptr ), ptr } );
     res->computed = true;
     return res;
+}
+
+template<class F,class...A>
+void Task::run( const F &func, A& ...args ) {
+    constexpr bool void_ret = std::is_same<decltype( func( args... ) ),void>::value;
+    run_void_or_not( std::integral_constant<bool,void_ret>(), func, args... );
+}
+
+template<class F,class...A>
+void Task::run_void_or_not( std::integral_constant<bool,0>, const F &func, A& ...args ) {
+    auto ret = func( args... );
+    outputs.push_back( { type_name( ret ), ret } );
+}
+
+template<class F,class...A>
+void Task::run_void_or_not( std::integral_constant<bool,1>, const F &func, A& ...args ) {
+    func( args... );
 }
 
 } // namespace parex
