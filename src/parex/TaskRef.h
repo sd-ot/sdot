ac3a79aed756beb5dfddbbdfd12318802488378f
Hugo Leclerc <hugal.leclerc@gmail.com>
2020-11-08 08:33:32 +0100

    nout

--- src/parex/TaskRef.h
+++ src/parex/TaskRef.h
@@ -13,13 +13,13 @@ namespace parex {
 class TaskRef {
 public:
     /**/        TaskRef   ( Task *t = nullptr, std::size_t nout = 0 ) : task( t ), nout( nout ) { inc_ref( t ); }
-    /**/        TaskRef   ( const TaskRef &that ) : task( that.task ) { inc_ref( task ); }
-    /**/        TaskRef   ( TaskRef &&that ) : task( std::exchange( that.task, nullptr ) ) {}
+    /**/        TaskRef   ( const TaskRef &that ) : task( that.task ), nout( that.nout ) { inc_ref( task ); }
+    /**/        TaskRef   ( TaskRef &&that ) : task( std::exchange( that.task, nullptr ) ), nout( that.nout ) {}
 
     /**/       ~TaskRef   () { dec_ref( task ); }
 
-    TaskRef&    operator= ( const TaskRef &that ) { inc_ref( that.task ); dec_ref( task ); task = that.task; return *this; }
-    TaskRef&    operator= ( TaskRef &&that ) { task = std::exchange( that.task, nullptr ); return *this; }
+    TaskRef&    operator= ( const TaskRef &that ) { inc_ref( that.task ); dec_ref( task ); task = that.task; nout = that.nout; return *this; }
+    TaskRef&    operator= ( TaskRef &&that ) { task = std::exchange( that.task, nullptr ); nout = that.nout; return *this; }
 
     static void inc_ref   ( Task *task ) { if ( task ) ++task->cpt_use; }
     static void dec_ref   ( Task *task ) { if ( task && ! --task->cpt_use ) delete task; }
