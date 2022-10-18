dd7281bac85b656975c8bb84f6ba2746e1f7c77e
Hugo Leclerc <hugal.leclerc@gmail.com>
2022-10-18 15:10:00 +0200

    win comp

--- src/sdot/Integration/SpaceFunctions/Polynomial.h
+++ src/sdot/Integration/SpaceFunctions/Polynomial.h
@@ -33,15 +33,17 @@ template<class TF,class Func>
 auto apply_poly( N<2>, const TF *coeffs, std::size_t delta_coeffs, std::size_t nb_coeffs, Func &&func ) {
     if ( nb_coeffs == 1 )
         return func( Constant<TF>{ coeffs[ 0 ] } );
-    if ( nb_coeffs == 6 )  // 1 x y xx xy yy
-        return func( Polynomial<TF,6>{ .coeffs = {
-            coeffs[ 0 * delta_coeffs ],
-            coeffs[ 1 * delta_coeffs ],
-            coeffs[ 2 * delta_coeffs ],
-            coeffs[ 3 * delta_coeffs ],
-            coeffs[ 4 * delta_coeffs ],
-            coeffs[ 5 * delta_coeffs ]
-        } } );
+    if ( nb_coeffs == 6 ) {
+        // 1 x y xx xy yy
+        Polynomial<TF,6> pf;
+        pf.coeffs[ 0 ] = coeffs[ 0 * delta_coeffs ];
+        pf.coeffs[ 1 ] = coeffs[ 1 * delta_coeffs ];
+        pf.coeffs[ 2 ] = coeffs[ 2 * delta_coeffs ];
+        pf.coeffs[ 3 ] = coeffs[ 3 * delta_coeffs ];
+        pf.coeffs[ 4 ] = coeffs[ 4 * delta_coeffs ];
+        pf.coeffs[ 5 ] = coeffs[ 5 * delta_coeffs ];
+        return func( pf );
+    }
     TODO;
 }
 
